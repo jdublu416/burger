f450fc67eefe4f5057bcde2ded2771df0cc22644
Janet Warren <jdublu416@gmail.com>
2018-05-07 22:02:37 -0400

    heroku code addition to connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,20 @@
 
 //    * Export the connection.
 var mysql = require("mysql");
+var connection;
 
+if (process.env.JAWSDB_URL){
+  connection= mysql.createConnection(process.env.JAWSDB_URL)
+}else{
+  
 var connection = mysql.createConnection({
-  port: 3307,
+  
   host: "localhost",
   user: "root",
   password: "Pirahn437",
   database: "burgers_db"
 });
-
+}
 // Make connection.
 connection.connect(function(err) {
   if (err) {
@@ -19,6 +24,7 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
+
 
 // Export connection for our ORM to use.
 module.exports = connection;
